cc95c8e689acc0bbbbd3e516f88dd0b111c60e3a
fxingan <112965322+fxingan@users.noreply.github.com>
2023-10-23 09:45:03 +0800

    代码上传

---
+++ ex1/RSA生成器/RSA生成器/RSA生成器.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+long long int mod_pow(long long int x, long long int e, long long int n) {
+    long long int result = 1;
+    while (e > 0) {
+        if (e % 2 == 1) {
+            result = (result * x) % n;
+        }
+        x = (x * x) % n;
+        e = e / 2;
+    }
+    return result;
+}
+
+int main() {
+    long long int n, p, q, e, t = 0, i, j = 0;
+    vector<long long int> s(1000); 
+    vector<long long int> z(1000);  
+    cin >> n >> p >> q >> e >> s[0] >> i;
+    while (1) {
+        s[j + 1] = mod_pow(s[j], e, n);
+        if (j < i) {
+            z[j] = s[j + 1] % 2;
+        }
+        if (t == 0) {
+            for (int k = 0; k <= j; k++) {
+                if (s[k] == s[j + 1]) {
+                    t = j + 1 - k;
+                    break;
+                }
+            }
+        }
+        if (t != 0 && j >= i) {
+            break;
+        }
+        j++;
+    }
+    cout << t << endl;
+    for (j = 0; j < i; j++) {
+        cout << z[j];
+    }
+    return 0;
+}
---
+++ ex2/Elgamal签名/Elgamal签名/Elgamal签名.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+using namespace std;
+
+int mod_pow(int x, int e, int n) {
+	int result = 1;
+	while (e > 0) {
+		if (e % 2 == 1) {
+			result = (result * x) % n;
+		}
+		x = (x * x) % n;
+		e = e / 2;
+	}
+	return result;
+}
+
+int find(int x, int y, int mod) {
+	if (y == 1) {
+		return 0;
+	}
+	int a = 1;
+	int result = x % mod;
+	while (result != y) {
+		result = (result * x) % mod;
+		a++;// 如果 a 增长到 mod 仍未找到 y，那么 y 不可能是 x^a % mod
+		if (a == mod) {
+			return -1; // 表示没有找到
+		}
+	}
+	return a;
+}
+
+int verify(int g, int q, int p, int m, int x, int y) {
+	int z1 = mod_pow(q, x, p);
+	int z2 = mod_pow(x, y, p);
+	int z = mod_pow(g, m, p);
+	if (z1 * z2 % p == z) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+	return 0;
+}
+
+int main() {
+	int p, g, q, m, x, y;
+	cout << "Please input:" << endl;
+	cin >> p >> g >> q >> m >> x >> y;
+	int a = find(g, q, p);
+	int r = find(g, x, p);
+	cout << "Output:" << endl;
+	verify(g, q, p, m, x, y);
+	cout << a << endl << r;
+	return 0;
+}
