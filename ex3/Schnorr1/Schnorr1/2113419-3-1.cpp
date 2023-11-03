88f7e57a0686de65fa93c5a7b224a60b8f7fdb4d
fxingan <2450521935@qq.com>
2023-11-03 10:29:55 +0800

    ex3

---
+++ ex3/Schnorr1/Schnorr1/2113419-3-1.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+int mod_pow(int x, int e, int n) {
+	int result = 1;
+	while (e > 0) {
+		if (e % 2 == 1) {
+			result = (result * x) % n;
+		}
+		x = (x * x) % n;
+		e = e / 2;
+	}
+	return result;
+}
+
+int main() {
+	int g, q, a, k, r;
+	cin >> g >> q >> a >> k >> r;
+	int p = 2 * q + 1;
+	int v = mod_pow(g, q - a, p);
+	int message = mod_pow(g, k, p);
+	int y = (k + a * r) % q;
+	cout << v << " " << message << " " << y;
+	return 0;
+}
